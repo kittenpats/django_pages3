2433a7d2e446daa89508073e1f326b18409a8850
Carla Elson <carlalatasha7@gmail.com>
2023-11-07 00:20:25 -0600

    initial commit

---
+++ django_pages3/urls.py
@@ -0,0 +1,8 @@
+# django_pages3/urls.py
+from django.contrib import admin
+from django.urls import path, include # new
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('pages.urls')), # new
+]
